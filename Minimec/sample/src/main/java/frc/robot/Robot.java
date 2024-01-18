f4a4a1837506a15b603215eda3ebca8f2b45026f
Matt Snyder <slylock2@mac.com>
2024-01-17 23:25:53 -0600

    sample plus gyro

---
+++ Minimec/sample/src/main/java/frc/robot/Robot.java
@@ -0,0 +1,60 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.util.sendable.SendableRegistry;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.drive.MecanumDrive;
+import edu.wpi.first.wpilibj.motorcontrol.Talon;
+import com.kauailabs.navx.frc.AHRS;
+
+/** This is a demo program showing how to use Mecanum control with the MecanumDrive class. */
+public class Robot extends TimedRobot {
+  private static final int kFrontLeftChannel = 1;
+  private static final int kRearLeftChannel = 0;
+  private static final int kFrontRightChannel = 3;
+  private static final int kRearRightChannel = 2;
+
+  private static final int kJoystickChannel = 0;
+
+  private MecanumDrive m_robotDrive;
+  private Joystick m_stick;
+
+  private final AHRS m_gyro = new AHRS(SPI.Port.kMXP, (byte) 200);
+
+  @Override
+  public void robotInit() {
+    Talon frontLeft = new Talon(kFrontLeftChannel);
+    Talon rearLeft = new Talon(kRearLeftChannel);
+    Talon frontRight = new Talon(kFrontRightChannel);
+    Talon rearRight = new Talon(kRearRightChannel);
+
+    SendableRegistry.addChild(m_robotDrive, frontLeft);
+    SendableRegistry.addChild(m_robotDrive, rearLeft);
+    SendableRegistry.addChild(m_robotDrive, frontRight);
+    SendableRegistry.addChild(m_robotDrive, rearRight);
+
+    // Invert the right side motors.
+    // You may need to change or remove this to match your robot.
+    frontRight.setInverted(true);
+    rearRight.setInverted(true);
+
+    m_robotDrive = new MecanumDrive(frontLeft::set, rearLeft::set, frontRight::set, rearRight::set);
+
+    m_stick = new Joystick(kJoystickChannel);
+  }
+
+  @Override
+  public void teleopPeriodic() {
+    // Use the joystick Y axis for forward movement, X axis for lateral
+    // movement, and Z axis for rotation.
+    Rotation2d gyroRotation = new Rotation2d(Units.degreesToRadians(m_gyro.getAngle()));
+    m_robotDrive.driveCartesian(-m_stick.getY(), -m_stick.getX(), -m_stick.getZ(), gyroRotation);
+  }
+}
